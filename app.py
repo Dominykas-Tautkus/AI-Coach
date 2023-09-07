7e2dd11ed439e24712498b0d2a3fe5f741d2cdc1
Dominykas-Tautkus <dominykast+2@gmail.com>
2023-09-07 15:02:50 +0300

    AI-Coach 1.0

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+import os
+import logging
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+DEBUG = os.environ.get('FLASK_DEBUG', False)  # Default is False if not set
+
+openai_api_key = os.environ.get('OPENAI_API_KEY', 'undefined')
+openai.api_key = openai_api_key 
+
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+    app.debug = True
+else:
+    logging.basicConfig(level=logging.WARNING)
+    app.debug = False
+
+chat_history = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/message', methods=['POST'])
+def message():
+    chat_history = [] # For the current application previous assistant context is not needed.
+    action = request.json['action']
+    system_prompt = "I want you to act as a joint personality of Tony Robbins and Brendon Burchard. Only ever use their own real ideas and advices. Speak conversationally, just like they would. Do not ever mention their names. All of your responses must be concise and brief. "
+
+    if len(chat_history) == 0:
+        chat_history.append({"role": "system", "content": system_prompt})
+
+    special_prompt = ""
+    if action == "Motivate Me":
+        special_prompt = "Enrich me with more motivation and drive please. Form it in an easy to read way with clear steps to take."
+    elif action == "Encourage Me":
+        special_prompt = "Say some words of encouragement please. Give me tips for overcoming and dealiing with disappointment."
+    elif action == "Cheer Me Up":
+        special_prompt = "Say something cheerful which would lighten me up please. Something funny and positive a performance coach guru would say. It's crucial for this to be uplifting."
+    elif action == "Energize Me":
+        special_prompt = "Say something energizing. To lift my energy up for taking action. I need some ideas on how to be (or feel) energized please."
+    elif action == "challenge":
+        special_prompt = "Say something to help deal with the difficult challenge ahead. It's important that you give the tips on how to be the most effective and best version of yourself when dealing with it."
+    elif action == "adversity":
+        special_prompt = "How to deal with adversity? It's important that you provide me support and effective ways and tips pf dealing with it."
+    elif action == "productivity":
+        special_prompt = "How could I improve my productivity? Tell me how can I be more effictive and do more things in the same amount of time."
+    else:
+        special_prompt = "Error."
+
+    if special_prompt:
+        chat_history.append({"role": "system", "content": special_prompt})
+
+    try:
+        gpt4_response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=chat_history,
+            max_tokens=300, 
+			top_p = 0.85, 
+			temperature = 1.4
+        )
+        bot_response = gpt4_response['choices'][0]['message']['content'].strip()
+        chat_history.append({"role": "assistant", "content": bot_response}) 
+        return jsonify(response=bot_response)
+    except Exception as e:
+        logging.exception(f"API error occurred: {e}")
+        return jsonify(error=str(e)), 500
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
---
+++ static/main.js
@@ -0,0 +1,56 @@
+let previousBotResponse;
+let previousUserAction;
+
+function replacePreviousElement(newElement, previousElement) {
+    if (previousElement) {
+        previousElement.remove();
+    }
+    return newElement;
+}
+
+async function handleAction(action) {
+    const chatBox = document.getElementById('chat-box');
+    const loadingSpinner = document.getElementById('loading-spinner');
+  
+    // Show the loading spinner
+    loadingSpinner.style.display = "block";
+  
+    // Add the user action to the chat box
+    const userActionDiv = document.createElement("div");
+    userActionDiv.innerHTML = `${action.toUpperCase()}`;
+  
+    previousUserAction = replacePreviousElement(userActionDiv, previousUserAction);
+    chatBox.appendChild(previousUserAction);
+
+    const payload = {
+        action: action
+    };
+
+    try {
+        const response = await fetch('http://127.0.0.1:5000/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        const data = await response.json();
+
+        // Hide the loading spinner
+        loadingSpinner.style.display = "none";
+    
+        // Add the bot's response to the chat box
+        const botResponseDiv = document.createElement("div");
+        botResponseDiv.id = 'bot-response';
+        botResponseDiv.innerHTML = `AI: ${data.response}`;
+  
+        previousBotResponse = replacePreviousElement(botResponseDiv, previousBotResponse);
+        chatBox.appendChild(previousBotResponse);
+        
+    } catch (e) {
+        // Hide the loading spinner in case of an error
+        loadingSpinner.style.display = "none";
+        console.error(e);
+    }
+}
