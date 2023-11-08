4dbf150757e52b9ad522f9ff727349005d494cfa
i-am-dom <dominykast+2@example.com>
2023-11-08 15:48:25 +0200

    v1.1: GPT4-Turbo-128k-Preview

--- app.py
+++ app.py
@@ -32,24 +32,25 @@ def index():
 def message():
     chat_history = [] # For the current application previous assistant context is not needed.
     action = request.json['action']
-    system_prompt = "I want you to act as a joint personality of Brendon Burchard, Tony Robbins, Sadhguru and Jim Rohn. Only ever use their own real ideas and advices. Speak conversationally, just like they would. Do not ever mention their names. All of your responses must be concise and brief. "
+	
+    system_prompt = "I want you to act as a joint personality of Brendon Burchard, Tony Robbins, Sadhguru and Jim Rohn. Only ever use their own real ideas and advices. Speak conversationally, just like they would. Do not ever mention their names. All of your responses must be concise and very brief. Do not apologize for confusion. "
 
     if len(chat_history) == 0:
         chat_history.append({"role": "system", "content": system_prompt})
 
     special_prompt = ""
     if action == "Motivate Me":
-        special_prompt = "Enrich me with strong motivation and drive please. Form it in an easy to read empowering way with very clear steps to take."
+        special_prompt = "Enrich me with strong motivation and drive please. Form it in an easy to read empowering way with very clear steps to take. Be concise"
     elif action == "Encourage Me":
         special_prompt = "Say some words of encouragement please. Give me tips for overcoming and dealiing with disappointment."
     elif action == "Cheer Me Up":
         special_prompt = "Say something cheerful which would lighten me up please. Something immediately positive. It's crucial for this to be uplifting and it needs to improve my mood effectively."
     elif action == "Energize Me":
-        special_prompt = "Say something energizing. To lift my energy up for taking action. I need some ideas on how to be (or feel) more energized please."
+        special_prompt = "Say something energizing. To lift my energy up for taking action and make me energized. I need some ideas on how to be (or feel) more energized please. Be concise and keep it brief"
     elif action == "challenge":
         special_prompt = "Say something to help deal with the difficult challenge ahead. It's important that you give the tips on how to be the most effective and best version of yourself when dealing with it."
     elif action == "adversity":
-        special_prompt = "How to deal with adversity? It's important that you provide me support and effective ways + tips of dealing with it."
+        special_prompt = "How to deal with adversity? It's important that you provide me support and effective ways + tips of dealing with it. Be concise"
     elif action == "productivity":
         special_prompt = "How could I improve my productivity? Tell me how can I be more effictive and get more things done in the same amount of time."
     else:
@@ -60,11 +61,11 @@ def message():
 
     try:
         gpt4_response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-4-1106-preview",
             messages=chat_history,
-            max_tokens=300, 
+            max_tokens=400, 
 			top_p = 0.85, 
-			temperature = 1.4
+			temperature = 1.3
         )
         bot_response = gpt4_response['choices'][0]['message']['content'].strip()
         chat_history.append({"role": "assistant", "content": bot_response}) 
